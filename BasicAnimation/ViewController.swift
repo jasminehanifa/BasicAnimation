43d4653c2829b2a2a3685d9837d024fa692842f6
Jasmine Hanifa Mounir <jasmine.mounir@gmail.com>
2019-05-17 15:41:06 +0700

    Frame and Bound

--- BasicAnimation/ViewController.swift
+++ BasicAnimation/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var superView: UIView!
+    @IBOutlet weak var blueSquare: UIView!
+    @IBOutlet weak var redSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        superView.isHidden = true
+//        UIView.animate(withDuration: 1.5, animations: {
+//                self.viewFrame.frame = CGRect(x: 70, y: 70, width: 200, height: 200)
+//            })
+//
+        print(blueSquare.bounds)
+        UIView.animate(withDuration: 1, animations: {
+            //frame
+            self.blueSquare.frame = CGRect(x: 30 , y: 30, width: 100, height: 100)
+            //bounds
+            self.redSquare.bounds = CGRect(x: 30, y: 30, width: 100, height: 100)
+            
+//            //superview
+//            self.superView.frame = CGRect(x: 30, y: 30, width: 300, height: 300)
+            print(self.blueSquare.bounds)
+            })
+        
+//    superView.addSubview(blueSquare)
+//    superView.addSubview(redSquare)
+        
+        
+        
+        
     }
 
 
